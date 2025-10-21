7aedc15fe7522a11b5b2a896f8325a1a177c9198
mariatorresd-star <maria.torres.d@utec.edu.pe>
2025-10-21 18:27:15 -0500

    baseline

---
+++ Hack1-base/src/main/java/com/example/hack1base/Hack1BaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hack1base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hack1BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hack1BaseApplication.class, args);
+    }
+
+}
---
+++ Hack1-base/src/test/java/com/example/hack1base/Hack1BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hack1base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack1BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
