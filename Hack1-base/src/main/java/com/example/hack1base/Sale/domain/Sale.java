47e9140f03bc491378445864f756dce965905dc4
eaus2006 <eurymar.umbria@utec.edu.pe>
2025-10-21 18:57:39 -0500

    entidades

---
+++ Hack1-base/src/main/java/com/example/hack1base/Sale/domain/Sale.java
@@ -0,0 +1,39 @@
+package com.example.hack1base.Sale.domain;
+
+import com.example.hack1base.User.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sales")
+@Getter
+@Setter
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String sku;
+
+    @Column(nullable = false)
+    private int units;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String branch;
+
+    @Column(nullable = false)
+    private LocalDateTime soldAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User createdBy;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ Hack1-base/src/main/java/com/example/hack1base/User/domain/User.java
@@ -0,0 +1,39 @@
+package com.example.hack1base.User.domain;
+
+import com.example.hack1base.Sale.domain.Sale;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String branch; // solo si es BRANCH
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "createdBy", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Sale> sales;
+}
